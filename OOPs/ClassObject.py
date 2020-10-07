c8410ea56df5ac5312c7990dfe5e6f9a3ea80b16
akshitone <akshitone8@gmail.com>
2020-10-07 16:48:35 +0530

    oops concepts in py

---
+++ OOPs/ClassObject.py
@@ -0,0 +1,11 @@
+class Computer:
+    def config(self):
+        print('''ASUS VIVOBOOK S15
+        PROCESSER: i5
+        RAM: 8GB
+        ROM: 1TB
+        GRAPHIC CARD: 2GB NVIDIA MX130''')
+
+
+computer = Computer()
+computer.config()
